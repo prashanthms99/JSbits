bf49483a2d8022e9c622be474d28094b3c380a06
prashanthms99 <prashanth.agni@gmail.com>
2019-04-14 12:37:00 +0530

    re-structured

---
+++ Basics/helloJS/hello.js
@@ -0,0 +1,10 @@
+//1 . Console
+console.log("Hello JS");
+//2
+document.querySelector('#querySelector').textContent='Hello JS';
+//3
+document.getElementById('getElementByID').textContent='Hello JS';
+//4
+//document.getElementById('getElementByID').textContent=prompt("Enter Text");
+//alert("Hello JS");
+
